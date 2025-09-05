0a7000f2b191556360c483774f1b738ef40b447b
Said Zúñiga <saidazg_lego@hotmail.com>
2025-09-05 05:15:27 -0600

    feat(ProductListScreen) : Add title to screen.

--- app/src/main/java/com/example/itgproductapiapp/ui/screens/productList/ProductListScreen.kt
+++ app/src/main/java/com/example/itgproductapiapp/ui/screens/productList/ProductListScreen.kt
@@ -1,8 +1,10 @@
 package com.example.itgproductapiapp.ui.screens.productList
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,9 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.itgproductapiapp.data.api.model.toJson
 import com.example.itgproductapiapp.ui.components.ProductCard
+import com.example.itgproductapiapp.ui.theme.productName
 
 @Composable
 fun ProductListScreen(
@@ -30,24 +34,37 @@ fun ProductListScreen(
         modifier = Modifier.fillMaxSize(),
     ) {
         if (state.value.isLoading) {
-            Text(modifier = Modifier.align(Alignment.Center), text = "Cargando...")
-        }else{
+            Text(modifier = Modifier.align(Alignment.Center), text = "Obteniendo productos...")
+        } else {
             if (state.value.error != null) {
                 Text(text = state.value.error!!)
             } else {
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize().align(Alignment.TopCenter),
-                    contentPadding = PaddingValues(16.dp),
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .align(Alignment.TopCenter),
                     horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    items(state.value.products.size) { index ->
-                        val product = state.value.products[index]
-                        ProductCard(
-                            producto = product,
-                            onItemClick = {
-                                showProduct(product.toJson())
-                            }
+                ) {
+                    Text(
+                        modifier = Modifier.padding(8.dp),
+                        text = "Lista de productos",
+                        style = productName.copy(
+                            fontSize = 22.sp
                         )
+                    )
+                    LazyColumn(
+                        contentPadding = PaddingValues(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        items(state.value.products.size) { index ->
+                            val product = state.value.products[index]
+                            ProductCard(
+                                producto = product,
+                                onItemClick = {
+                                    showProduct(product.toJson())
+                                }
+                            )
+                        }
                     }
                 }
             }
